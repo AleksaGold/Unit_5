8933dd8cea02bbeae93076923a07f14846c505a2
Alexandra Goldobina <goldobina.s@gmail.com>
2024-07-22 17:19:50 +0300

    1. добавлено закрытие подключения

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from config import config, EMPLOYERS
-import src.DBManager
+from src.DBManager import DBManager
 from src.api import HhAPI
 from src.utils import create_tables, get_employers, save_data_to_database
 
@@ -17,7 +17,7 @@ def main():
     create_tables(params)
     save_data_to_database(employers, vacancies, params)
 
-    data_base = src.DBManager.DBManager(params)
+    data_base = DBManager(params)
 
     print("\nВведите ваш номер вашего запроса:\n"
           "1 - Список компаний и количество вакансий\n"
@@ -28,31 +28,35 @@ def main():
 
     user_request = int(input())
 
-    if user_request == 1:
-        response_to_user_request = data_base.get_companies_and_vacancies_count()
-        print(response_to_user_request)
-        for row in response_to_user_request:
-            print(row)
-    elif user_request == 2:
-        response_to_user_request = data_base.get_all_vacancies()
-        print(response_to_user_request)
-        for row in response_to_user_request:
-            print(row)
-    elif user_request == 3:
-        response_to_user_request = data_base.get_avg_salary()
-        print(response_to_user_request)
-    elif user_request == 4:
-        response_to_user_request = data_base.get_vacancies_with_higher_salary()
-        print(response_to_user_request)
-        for row in response_to_user_request:
-            print(row)
-    elif user_request == 5:
-        keyword = input("Введите ключевое слово для поиска вакансий\n")
-        response_to_user_request = data_base.get_vacancies_with_keyword(keyword)
-        for row in response_to_user_request:
-            print(row)
-    else:
-        print('Введен неверный запрос. Попробуйте снова')
+    try:
+        if user_request == 1:
+            response_to_user_request = data_base.get_companies_and_vacancies_count()
+            print(response_to_user_request)
+            for row in response_to_user_request:
+                print(row)
+        elif user_request == 2:
+            response_to_user_request = data_base.get_all_vacancies()
+            print(response_to_user_request)
+            for row in response_to_user_request:
+                print(row)
+        elif user_request == 3:
+            response_to_user_request = data_base.get_avg_salary()
+            print(response_to_user_request)
+        elif user_request == 4:
+            response_to_user_request = data_base.get_vacancies_with_higher_salary()
+            print(response_to_user_request)
+            for row in response_to_user_request:
+                print(row)
+        elif user_request == 5:
+            keyword = input("Введите ключевое слово для поиска вакансий\n")
+            response_to_user_request = data_base.get_vacancies_with_keyword(keyword)
+            for row in response_to_user_request:
+                print(row)
+        else:
+            print('Введен неверный запрос. Попробуйте снова')
+    finally:
+        data_base.cur.close()
+        data_base.conn.close()
 
 
 if __name__ == '__main__':
